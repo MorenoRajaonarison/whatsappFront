d8a9044e961f0eb180e8849533b60980c3d983d8
Rajaonarison <rajaonarison.moreno@gmail.com>
2023-09-06 17:47:34 +0200

    fix: protect the app route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import { useSelector } from "react-redux";
 
 function App() {
+  const { user } = useSelector((state) => state.user);
+  const { token } = user;
   return (
     <BrowserRouter>
       <div className="dark">
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
+          <Route
+            exact
+            path="/"
+            element={token ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            exact
+            path="/login"
+            element={!token ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            exact
+            path="/register"
+            element={!token ? <Register /> : <Navigate to="/" />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
