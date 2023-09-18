be050f77793a93ce8414c4ecb2cd6e7ee84b093f
Rajaonarison <rajaonarison.moreno@gmail.com>
2023-09-18 16:22:19 +0200

    feat: display messages

--- src/components/Chat/messages/ChatMessages.jsx
+++ src/components/Chat/messages/ChatMessages.jsx
@@ -1,9 +1,20 @@
+import { useSelector } from "react-redux";
+import Message from "./Message";
+
 const ChatMessages = () => {
+  const {messages} = useSelector(state => state.chat)
+const {user} = useSelector(state => state.user)
+
   return (
     <div className="mb-[60px] bg-[url('https://res.cloudinary.com/dmhcnhtng/image/upload/v1677358270/Untitled-1_copy_rpx8yb.jpg')] bg-cover bg-no-repeat">
       {/* container */}
       <div className="scrollbar overflow_scrollbar overflow-auto py-2 px-[6%]">
         {/* messages */}
+        {
+          messages && messages.map(msg => (
+            <Message message={msg} key={msg._id} me={user._id === msg.sender._id}/>
+          ))
+        }
       </div>
     </div>
   );
---
+++ src/components/Chat/messages/Message.jsx
@@ -0,0 +1,38 @@
+import moment from "moment/moment";
+import React from "react";
+
+const Message = ({ message, me }) => {
+  return (
+    <div
+      className={`w-full flex mt-2 space-x-3 max-w-xs ${
+        me ? "ml-auto justify-end" : ""
+      }`}
+    >
+      {/* message container */}
+      <div className="">
+        <div
+          className={`relative h-full dark:text-dark_text_1 p-2 rounded-lg ${
+            me ? "bg-green_3" : "dark:bg-dark_bg_2"
+          }`}
+        >
+          {/* message */}
+          <p className="float-left h-full text-sm pb-4 pr-8">
+            {message.message}
+          </p>
+          {/* date */}
+          <span className="absolute right-1.5 bottom-1.5 text-xs  text-text-dark_text_5 leading-none">
+            {moment(message.createdAt).format("HH:mm")}
+          </span>
+          {/* traingle */}
+          {/* {!me ? (
+            <span>
+              <TraingleIcon className="dark:fill-dark_bg_2 rotate-[60deg absolute top-[-5px] -left-1.5]" />
+            </span>
+          ) : null} */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
