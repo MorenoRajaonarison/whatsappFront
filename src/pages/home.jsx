c6a7a40fd1f49aa7c01943d54784a18c45704ca5
Rajaonarison <rajaonarison.moreno@gmail.com>
2023-09-18 13:29:21 +0200

    feat: whatsapp main component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,10 +2,12 @@ import { useEffect } from "react";
 import Sidebar from "../components/sidebar/Sidebar";
 import { useDispatch, useSelector } from "react-redux";
 import { getConversations } from "../features/chatSlice";
+import WhatsappHome from "../components/Chat/Welcome/WhatsappHome";
 
 function Home() {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
+  const {activeConversation} = useSelector(state => state.chat)
 
   // get conversations
   useEffect(() => {
@@ -17,9 +19,12 @@ function Home() {
   return (
     <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
       {/* container */}
-      <div className="container h-screen flex w-full">
+      <div className="container h -screen flex w-full">
         {/* Sidebar */}
         <Sidebar />
+        {
+          activeConversation._id? 'homr': <WhatsappHome/>
+        }
       </div>
     </div>
   );
