c11628b5ea9ebf7374e21781919991000d472ad0
Rajaonarison <rajaonarison.moreno@gmail.com>
2023-09-24 18:42:12 +0200

    update: delete inutil state in socket context

--- src/context/socketContext.jsx
+++ src/context/socketContext.jsx
@@ -9,20 +9,18 @@ export const useSocket = () => {
 
 export const SocketProvider = ({ children, serverUrl }) => {
   const [socket, setSocket] = useState(null);
-  const [isSocketReady, setIsSocketReady] = useState(false);
 
   useEffect(() => {
     const newSocket = io(serverUrl);
     console.log("Socket initialized:", newSocket);
     setSocket(newSocket);
-    setIsSocketReady(true);
 
     return () => newSocket.close();
   }, [serverUrl]);
 
   return (
     <SocketContext.Provider value={socket}>
-      {isSocketReady && children}
+      {socket && children}
     </SocketContext.Provider>
   );
 };
